746c8989d5444f7a75e5b7da841c06452e2d1ade
muhammad-usman-1 <usmanansar251@gmail.com>
2026-09-21 12:05:01 +0500

    rebuild css assets

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(SettingsSeeder::class);
-        // $this->call(ProductSeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
